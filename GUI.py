36863e7dbbbb33071140f94c80c76a0a9610b36c
BuildTools <unconfigured@null.spigotmc.org>
2022-10-01 11:43:19 -0400

    linux error support + done with getting steam path

--- GUI.py
+++ GUI.py
@@ -21,6 +21,9 @@ SteamINS = ""
 Library = ""
 z = 0
 x3 = 0
+global ISDIR
+ISDIR = False
+GUID = True
 global dir_name
 dir_name= ""
 SteamLibrary = []
@@ -28,9 +31,9 @@ sg.theme('dark grey 9')
 dir = "./Replacement Images"
 ImgR = next(os.walk(dir))[2]
 imgC = len(ImgR)
-layout = [  [sg.Text('Number of images to insert:' + str(imgC))],
+layout = [  [sg.Text('Number of images to insert:' + str(imgC)), sg.Checkbox("Dev",key='_DCH_')],
             [sg.Text('Seed for randomness:'), sg.InputText('',key='_h_'), sg.Button('Use Default')], #move seed settings to settings tab and auto-set seed to default.
-            [sg.Button('Run!'), sg.Button('Quit')] ]
+            [sg.Button('Run!'), sg.Button('Quit'), sg.Button('GUILibSettings', key='_GUIL_',disabled=GUID)] ]
 layoutS = [  [sg.Text('Please wait attempting to get STI directory.')],
              [sg.Image(data=gif103, key='_IMAGE_', pad=(100,0))], #move seed settings to settings tab and auto-set seed to default.
              [sg.Button('Quit',key='_h2_'), sg.ProgressBar(10,'horizontal',s=(10,10),bar_color=("blue","white"),expand_x=True,key='progressbar')] ]            
@@ -71,64 +74,77 @@ try:
     LibraryJ1 = json.loads(FO.read())
     FO.close()
     progress_bar.Update(x+1.5) #6.5
+    print("firrst except end")
 except FileNotFoundError:
-    SteamReg = winreg.OpenKey(key=winreg.HKEY_LOCAL_MACHINE,sub_key="SOFTWARE\Wow6432Node\Valve\Steam",reserved=0,access=winreg.KEY_READ)
-    progress_bar.Update(x+0.5) #0.5
-    SteamINSD = winreg.QueryValueEx(SteamReg, "InstallPath")
-    progress_bar.Update(x+0.5) #1
-    SteamST = str(SteamINSD)
-    SteamST = SteamST.removeprefix("('")
-    SteamST = SteamST.removesuffix("', 1)")
-    progress_bar.Update(x+1) #2
-    SteamINS = SteamST
-    #install directory gotten, just need to go into the steamapps folder and get libraryfolders.??? and parse it as json, get the id and in what path it is, then path trought
-    #those files for the image folder.
-    Library = SteamST + "\\steamapps\\libraryfolders.vdf"
-    progress_bar.Update(x+0.5) #2.5
-    LibraryP = vdf.parse(open(Library))
-    progress_bar.Update(x+0.5) #3
-    fO = open("./Library.json", "w")
-    fO.write(str(LibraryP))
-    fO.close()
-    progress_bar.Update(x+0.5)#3.5
-    fO2 = open("./Library.json", "r")
-    filedata = fO2.read()
-    fO2.close()
-    filedata = filedata.replace('\'', '\"')
-    progress_bar.Update(x+0.5)#4
-    fO3 = open("./Library.json", "w")
-    fO3.write(filedata)
-    fO3.close()
-    progress_bar.Update(x+1)#5
-    FO = open("./Library.json", "r")
-    LibraryJ1 = json.loads(FO.read())
-    FO.close()
-    progress_bar.Update(x+1.5) #6.5
-    while True:
-        event, values = window2.read(timeout=50)
-        try:
-            SteamLibrary.append(LibraryJ1['libraryfolders'][str(y)]['path'])
-            y = int(y) + 1
-            print("z" + str(z))
-            print("y" + str(y))
-            progress_bar.Update(x+0.25)
-            x2 = x2 + 0.25
-            print(x2)
-        except KeyError:
-            print("keyerror")
-            ISDIR = False
-            while True:
-                print("testing dir")
-                event, values = window2.read(timeout=50)
-                try:                                        # this down here gives IndexError!
-                    ISDIR = os.path.isdir(str(SteamLibrary[z]) + "\\steamapps\\common\\The Jackbox Party Pack 4\\games\\SurviveTheInternet\\content\\STIPhoto")
-                    z = z + 1
-                    print(z)
-                    print(ISDIR)
-                except IndexError:
-                    dir_name = str(SteamLibrary[z]) + "\\steamapps\\common\\The Jackbox Party Pack 4\\games\\SurviveTheInternet\\content\\STIPhoto"
+    try:
+        SteamReg = winreg.OpenKey(key=winreg.HKEY_LOCAL_MACHINE,sub_key="SOFTWARE\Wow6432Node\Valve\Steam",reserved=0,access=winreg.KEY_READ)
+        progress_bar.Update(x+0.5) #0.5
+        SteamINSD = winreg.QueryValueEx(SteamReg, "InstallPath")
+        progress_bar.Update(x+0.5) #1
+        SteamST = str(SteamINSD)
+        SteamST = SteamST.removeprefix("('")
+        SteamST = SteamST.removesuffix("', 1)")
+        progress_bar.Update(x+1) #2
+        SteamINS = SteamST
+        #install directory gotten, just need to go into the steamapps folder and get libraryfolders.??? and parse it as json, get the id and in what path it is, then path trought
+        #those files for the image folder.
+        Library = SteamST + "\\steamapps\\libraryfolders.vdf"
+        progress_bar.Update(x+0.5) #2.5
+        LibraryP = vdf.parse(open(Library))
+        progress_bar.Update(x+0.5) #3
+        fO = open("./Library.json", "w")
+        fO.write(str(LibraryP))
+        fO.close()
+        progress_bar.Update(x+0.5)#3.5
+        fO2 = open("./Library.json", "r")
+        filedata = fO2.read()
+        fO2.close()
+        filedata = filedata.replace('\'', '\"')
+        progress_bar.Update(x+0.5)#4
+        fO3 = open("./Library.json", "w")
+        fO3.write(filedata)
+        fO3.close()
+        progress_bar.Update(x+1)#5
+        FO = open("./Library.json", "r")
+        LibraryJ1 = json.loads(FO.read())
+        FO.close()
+        progress_bar.Update(x+1.5) #6.5
+        print("firrst except end")
+        while True:
+            event, values = window2.read(timeout=50)
+            try:
+                SteamLibrary.append(LibraryJ1['libraryfolders'][str(y)]['path'])
+                y = int(y) + 1
+                print("z" + str(z))
+                print("y" + str(y))
+                progress_bar.Update(x+0.25)
+                x2 = x2 + 0.25
+                print(x2)
+            except KeyError:
+                print("keyerror")
+                if ISDIR == True:
                     break
-                   
+                while ISDIR == False:
+                    print("testing dir")
+                    try:                                        
+                        print(z)
+                        ISDIR = os.path.isdir(str(SteamLibrary[z]) + "\\steamapps\\common\\The Jackbox Party Pack 4\\games\\SurviveTheInternet\\content\\STIPhoto")
+                        z = z + 1
+                        print(z)
+                        print(ISDIR)
+                    except IndexError:
+                        print("indexerror")
+                        z = z - 1
+                        dir_name = str(SteamLibrary[z]) + "\\steamapps\\common\\The Jackbox Party Pack 4\\games\\SurviveTheInternet\\content\\STIPhoto"
+                        print("break")
+                        raise
+    except FileNotFoundError:
+        print("On linux. Tried to use GUI.")
+        sg.popup_error_with_traceback("STI Linux GUI Error","Hi! It seems as you are running the GUI script on linux.","Please follow the linux instructions in the Readme.")
+        exit(2)      
+time.sleep(1)
+progress_bar.Update(x+6)
+time.sleep(1)
 progress_bar.Update(x+10)
 time.sleep(1)     
 window2.close()       
@@ -150,4 +166,10 @@ while True:
         values[0] = str(34587345)
         print(values[0])
         window['_h_'].update(values[0])
+    if event == 'GUILibSettings':
+        sg.main_global_pysimplegui_settings()
+    if values['_DCH_'] == True:
+        print("dev")
+        GUID = False
+        
 window.close()
