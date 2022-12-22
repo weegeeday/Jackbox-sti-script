1e85bc718600ca27fe5dfacbc655020f15fa8cef
SCWeeegeeday <104513094+SCWeeegeeday@users.noreply.github.com>
2022-12-22 11:57:16 -0500

    Update Replacer.py

--- Replacer.py
+++ Replacer.py
@@ -1,165 +1,151 @@
-from PIL import Image
-from random import seed
-from random import randint
 import os
+import random
 import time
+from PIL import Image
+
 import PySimpleGUI as sg
-randnO = 0
-ImgNbR = 0
-randnR = 0
-tumbnb = 0
-SeeD = 0
-imgst = False
-dir = "./Replacement Images"
-dir_name2 = ".\Replacement Images"
-dir_name3 = ".\Images to put in STIPphoto"
-fO1 = open("./Seed.txt", "r")
-fO1.readline(1)
-seed(fO1.readline(1))
-fO1.close()
 
-class Replacer():
-    @staticmethod
-    def genintO():
-        global randnO
-        for _ in range(1):
-            value = randint(0, 131)
-            randnO = value
-            print("Random NBO genereated:")
-            print(randnO)
-            MRun.ImgCountR()
 
-    @staticmethod
-    def ImgCountR():
-        global ImgNbR
-        ImgR = next(os.walk(dir))[2]
+class Replacer:
+    def __init__(self, seeD):
+        self.dir_name = "./Replacement Images"
+        self.dir_name2 = ".\Replacement Images"
+        self.dir_name3 = ".\Images to put in STIPphoto"
+        self.image_number = 0
+        self.replacement_number = 0
+        self.original_image = ""
+        self.replacement_image = ""
+        self.imgst = False
+        self.seed = seeD
+
+    def genintO(self):
+        self.image_number = random.randint(0, 131)
+        print("Random NBO generated:")
+        print(self.image_number)
+        self.img_count_r()
+
+    def img_count_r(self):
+        images = next(os.walk(self.dir_name))[2]
         print("Number of Replacement Images Counted:")
-        ImgNbR = len(ImgR)
-        print(ImgNbR)
-        MRun.ImgFilterO()
+        self.replacement_count = len(images)
+        print(self.replacement_count)
+        self.img_filter_o()
 
-    @staticmethod
-    def ImgFilterO():
-        global dir_name
-        global DIR
-        fO = open("./IMGFO.txt", "w")
-        DIR = open("./dir_name.txt", "r")
-        dir_name = DIR.read()
-        print(dir_name)
+    def img_filter_o(self):
+        with open("./dir_name.txt", "r") as f:
+            self.dir_name = f.read()
+        print(self.dir_name)
         try:
-            ImgFO = sorted( filter( lambda x: os.path.isfile(os.path.join(dir_name, x)),
-                        os.listdir(dir_name) ) )
-            for file_name in ImgFO:
-                fO.write(file_name)
-                fO.write("\n")              
-            fO.close()
-            DIR.close()
-            MRun.ImgFilterR()
+            image_files = sorted(
+                filter(
+                    lambda x: os.path.isfile(os.path.join(self.dir_name, x)),
+                    os.listdir(self.dir_name),
+                )
+            )
+            with open("./IMGFO.txt", "w") as f:
+                for file_name in image_files:
+                    f.write(file_name)
+                    f.write("\n")
+            self.img_filter_r()
         except FileNotFoundError:
             print("invalid dir.")
-            sg.popup("Invalid directory was set!")           
-            MRun.crash()
+            sg.popup("Invalid directory was set!")
+            self.crash()
 
-    @staticmethod
-    def ImgFilterR():
-        fR = open("./IMGFR.txt", "w")
-        ImgFR = sorted( filter( lambda x: os.path.isfile(os.path.join(dir_name2, x)),
-                        os.listdir(dir_name2) ) )
-        for file_name in ImgFR:
-            fR.write(file_name)
-            fR.write("\n")              
-        fR.close()
-        MRun.genintR()
-    
-    @staticmethod
-    def genintR():
-        global randnR
-        for _ in range(1):
-            try:
-                value = randint(0, ImgNbR - 1)
-                randnR = value
-                print("Random NBR genereated:")
-                print(randnR)
-                MRun.ImgIMCREX()
-            except ValueError:
-                print("hello crash")
-                MRun.crash()
-#make new method for SRTool
-    @staticmethod
-    def ImgIMCREX():
-        global OImgO
-        global imgst
-        f = open("./IMGFO.txt", "r")
-        o = open("./IMGFR.txt", "r")
-        OImg = f.readlines()
-        OImgR = o.readlines()
-        OImgO = OImg[randnO].rstrip("\n")
-        OImgRO = OImgR[randnR].rstrip("\n")
-        im = Image.open(dir_name +"/" + OImgO)
-        imR = Image.open("./Replacement Images/" + OImgRO)
-        im.close()
-        imR = imR.resize((425, 320))
-        imR = imR.convert("RGB")
-        if not os.path.exists("./Images to put in STIPphoto/" + OImgO):
-            imgst = True
-            print("New image saved!")
-            imR.save("./Images to put in STIPphoto/" + OImgO)
-            os.replace("./Replacement Images/" + OImgRO, "./Replacement Images Backup/" + OImgRO)           
-            print("Replacement IMG used moved!")
-            MRun.tumb()
-        print("IMG already exists...")
-        imR.close()
-        MRun.reset()
-    
-    @staticmethod
-    def tumb():
+    def img_filter_r(self):
+        with open("./IMGFR.txt", "w") as f:
+            image_files = sorted(
+                filter(
+                    lambda x: os.path.isfile(os.path.join(self.dir_name2, x)),
+                    os.listdir(self.dir_name2),
+                )
+            )
+            for file_name in image_files:
+                f.write(file_name)
+                f.write("\n")
+        self.genint_r()
+
+    def genint_r(self):
+        try:
+            self.replacement_number = random.randint(0, self.replacement_count - 1)
+            print("Random NBR generated:")
+            print(self.replacement_number)
+            self.img_im_crex()
+        except ValueError:
+            print("hello crash")
+            self.crash()
+
+    def img_im_crex(self):
+        with open("./IMGFO.txt", "r") as f:
+            original_images = f.readlines()
+        with open("./IMGFR.txt", "r") as f:
+            replacement_images = f.readlines()
+        self.original_image = original_images[self.image_number].rstrip("\n")
+        self.replacement_image = replacement_images[self.replacement_number].rstrip("\n")
+
+        with Image.open(self.dir_name + "/" + self.original_image) as im:
+            with Image.open("./Replacement Images/" + self.replacement_image) as im_r:
+                im_r = im_r.resize((425, 320))
+                im_r = im_r.convert("RGB")
+                if not os.path.exists(
+                    "./Images to put in STIPphoto/" + self.original_image
+                ):
+                    im_r.save(
+                        "./Images to put in STIPphoto/" + self.original_image
+                    )
+                    self.imgst = True
+                else:
+                    self.imgst = False
+                    sg.popup("An image with the same name already exists!")
+                    self.crash()
+
+    def crash(self):
+        print("crash")
+        time.sleep(3)
+        exit()
+
+    def tumb(self):
         print("tumbnail method called")
-        OImgO2 = OImgO.rstrip(".jpg")
-        ImgtO = OImgO2 + "-thumb.jpg"   
-        imt = Image.open("./Images to put in STIPphoto/" + OImgO)
-        imt = imt.resize((120, 90))
-        imt = imt.convert("RGB")
-        imt.save("./Images to put in STIPphoto/Thumbnails/" + ImgtO)
-        print("Tumbnail saved!")
-        imt.close()
-        MRun.reset()
+        original_image_name = self.original_image.rstrip(".jpg")
+        thumbnail_name = original_image_name + "-thumb.jpg"
+        with Image.open(
+            "./Images to put in STIPphoto/" + self.original_image
+        ) as imt:
+            imt = imt.resize((120, 90))
+            imt = imt.convert("RGB")
+            imt.save("./Images to put in STIPphoto/Thumbnails/" + thumbnail_name)
+            print("Tumbnail saved!")
 
-            
-    
-    @staticmethod
-    def reset():
-        print(SeeD)
-        ImgR = next(os.walk(dir))[2]
-        if len(ImgR) == 0:
+    def reset(self):
+        print(self.seed)
+        images = next(os.walk(self.dir_name))[2]
+        if len(images) == 0:
             print("replacement img count == 0, crashing.")
-            MRun.crash()
-        elif len(ImgR) != 0:
+            self.crash()
+        elif len(images) != 0:
             print("Looping")
             time.sleep(1)
-            MRun.genintO()
-    
-    @staticmethod
-    def crash():
-        try:
-            print("crashing to return to GUI.py")
-            fR = open("./crash.txt", "r")
-            fR.readlines()
-        except FileNotFoundError:
-            print("hello fillenotfounderror")
-    @staticmethod
-    def SRTOOL(Reimg,Orimg):
-        global imgst
+            self.genintO()
+
+    def SRTOOL(self, reimg, orimg):
         DIR = open("./dir_name.txt", "r")
-        dir_name = DIR.read()
-        Pre = Image.open(str(dir + "/" + Reimg))
-        Dor = str(dir_name + "\\" + Orimg)
-        Pre = Pre.resize((425, 320))
-        Pre = Pre.convert("RGB")
-        Pre.save("./Images to put in STIPphoto/" + Orimg)
-        os.replace("./Replacement Images/" + Reimg, "./Replacement Images Backup/" + Reimg)
-        Pre = Pre.resize((120, 90))
-        Orimg2 = Orimg.rstrip(".jpg")
-        OrimgT = Orimg2 + "-thumb.jpg"        
-        Pre.save("./Images to put in STIPphoto/Thumbnails/" + OrimgT)          
-MRun = Replacer()
-#MRun = MRun.reset()
+        self.dir_name = DIR.read()
+        with Image.open(str(self.dir_name + "/" + reimg)) as pre:
+            pre = pre.resize((425, 320))
+            pre = pre.convert("RGB")
+            pre.save("./Images to put in STIPphoto/" + orimg)
+            os.replace(
+                "./Replacement Images/" + reimg,
+                "./Replacement Images Backup/" + reimg,
+            )
+            pre = pre.resize((120, 90))
+            orimg2 = orimg.rstrip(".jpg")
+            orimgT = orimg2 + "-thumb.jpg"
+            pre.save("./Images to put in STIPphoto/Thumbnails/" + orimgT)
+
+def main():
+    replacer = Replacer()
+    replacer.reset()
+
+if __name__ == "__main__":
+    main()
